2aa27978ad6da412e9f4233a565615b832e2a75e
tianhe.wang <tianhe.wang@ushow.media>
2018-07-31 16:02:46 +0800

    init p1 and p4

---
+++ leetcode/p1_TowSum.cc
@@ -0,0 +1,27 @@
+/**
+ * Problem: Two Sum
+ *
+ * Description: Given an array of integers, return indices of the two numbers
+ * such that they add up to a specific target. Note: You may assume that each
+ * input would have exactly one solution, and you may not use the same element
+ * twice.
+ *
+ * Example:
+ *   nums = [2, 7, 11, 15], target = 9
+ *   return [0, 1] (because nums[0] + nums[1] = 2 + 7 = 9)
+ */
+#include <vector>
+#include <iostream>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target)
+    {
+        return std::vector<int> (2, 0);
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
---
+++ leetcode/p4_MedianOfTwoSortedArray.cc
@@ -0,0 +1,34 @@
+/**
+ * Problem: Median of Two Sorted Array
+ *
+ * Description: There are two arrays nums1 and nums2 of size m and n
+ * respectively. Find the median of the two sorted arrays. The overall run time
+ * complexity should be O(log(m+n). Note: You may assume nums1 and nums2 can not be both empty.
+ *
+ * Example 1:
+ *   nums1 = [1, 3]
+ *   nums2 = [2]
+ *   The median is 2.0
+ *
+ *   nums1 = [1, 2]
+ *   nums2 = [3, 4]
+ *   The median is  (2 + 3) / 2 = 2.5
+ */
+#include <vector>
+#include <iostream>
+
+class Solution {
+public:
+    double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2)
+    {
+        return 0.0;    
+    }
+
+};
+
+int main(int argc, char* argv[])
+{
+    Solution solution;
+//    solution.findMedianSortedArrays();
+    return 0;
+}
