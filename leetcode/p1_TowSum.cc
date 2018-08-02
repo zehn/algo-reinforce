72c33d3a93f78f090cf0150d8fa8a623d554b4a0
tianhe.wang <tianhe.wang@ushow.media>
2018-08-02 15:03:55 +0800

    update p1

--- leetcode/p1_TowSum.cc
+++ leetcode/p1_TowSum.cc
@@ -11,17 +11,40 @@
  *   return [0, 1] (because nums[0] + nums[1] = 2 + 7 = 9)
  */
 #include <vector>
+#include <unordered_map>
 #include <iostream>
 
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target)
     {
-        return std::vector<int> (2, 0);
+        std::vector<int> location;
+        std::unordered_map<int, int> hash;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            int number_to_find = target - nums[i];
+
+            if (hash.end() != hash.find(number_to_find)) {
+                location.push_back(hash[number_to_find]);
+                location.push_back(i);
+                break;
+            }
+            hash[nums[i]] = i;
+        }
+
+        return location;
     }
 };
 
 int main(int argc, char* argv[])
 {
+    Solution solution;
+
+    int test_case[] = {2, 7, 11, 15};
+    std::vector<int> v(test_case, test_case + sizeof(test_case) / sizeof(int)); 
+    std::vector<int> result = solution.twoSum(v, 9);
+    for (int i = 0; i < result.size(); ++i)
+        std::cout << result[i] << ' ';
+    std::cout << '\n';
     return 0;
 }
